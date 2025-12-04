36210c64470cb8fbd17784136852def86b6aea06
Jairo Rodriguez <jairodri.a05@gmail.com>
2025-12-04 01:59:38 +0000

    backend funcional

--- api/ocr.js
+++ api/ocr.js
@@ -1,33 +1,32 @@
-// api/ocr.js
-import { recognize } from 'tesseract.js-node';
+import Tesseract from 'tesseract.js';
 
 export default async function handler(req, res) {
+
   if (req.method === "GET") {
     // Mensaje de prueba para acceder a la URL principal
     return res.status(200).json({ message: "Backend OCR funcionando en Vercel 🚀" });
   }
 
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Método no permitido" });
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Método no permitido' });
   }
 
   try {
     const { image } = req.body; // Base64 o URL
-
     if (!image) {
-      return res.status(400).json({ error: "Se requiere una imagen" });
+      return res.status(400).json({ error: 'Se requiere una imagen' });
     }
 
-    const result = await recognize(image, "spa", {
+    const { data } = await Tesseract.recognize(image, 'spa', {
       logger: m => console.log(m),
     });
 
     return res.status(200).json({
-      text: result.data.text,
+      text: data.text,
     });
 
   } catch (err) {
-    console.error("OCR Error:", err);
-    return res.status(500).json({ error: "Error procesando OCR" });
+    console.error('OCR Error:', err);
+    return res.status(500).json({ error: 'Error procesando OCR' });
   }
 }
