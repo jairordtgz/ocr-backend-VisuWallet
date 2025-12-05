e54d018956e11d0047c060af0d4c4ab50e764a5d
Jairo Rodriguez <jairodri.a05@gmail.com>
2025-12-05 18:08:30 +0000

    ojala funcione

--- api/analyze-image/index.js
+++ api/analyze-image/index.js
@@ -48,7 +48,7 @@ export default async function handler(req, res) {
               content: [
                 {
                   type: "input_image",
-                  image_url: imageBase64,
+                  image_base64: imageBase64,
                 },
               ],
             },
